c37c5bb434678d6d7224b66abdfcbbf5d8a35255
Túlio Canavesi Morato <tcmorato28@gmail.com>
2026-09-05 14:14:28 -0300

    Adição de vetor para armazenar as transferências e imprimir extrato, além de algumas correções na dinâmica de funcionamento do sistema.

--- Terminal de finanças/terminalFinance.cpp
+++ Terminal de finanças/terminalFinance.cpp
@@ -1,35 +1,63 @@
 #include <iostream>
 #include <iomanip>
+#include <vector>
 
 
+void limparTela(){
+    system("cls");
+}
 
-float addTransacao(float saldo) {
+void armazenarTransf(float transf, std::vector<float> & armazenador){
+    armazenador.push_back(transf);
+}
+
+
+//registra movimentação no saldo, seja adição ou subtração
+float addTransacao(float saldo, std::vector<float> & extrato) {
     float valorTransf;
     int tipoTransf;
     float novoSaldo;
+    bool processo = true;
 
-    std::cout << "Digite o valor: ";
-    std::cin >> valorTransf;
-    std::cout << "Qual é o tipo de transferência?\n\n"
-                 "1- Receita\n"
-                 "2- Despesa\n"
-                 "3- Voltar\n";
-    std::cin >> tipoTransf;
-    switch (tipoTransf)
+    while (processo == true)
     {
-    case 1:
-        novoSaldo = saldo + valorTransf;
-        std::cout << "Receita: +" << std::fixed << std::setprecision(2) << valorTransf << std::endl;
-        std::cout << "Saldo: " << std::fixed << std::setprecision(2) << novoSaldo << std::endl;
-        break;
-    case 2:
-        novoSaldo = saldo - valorTransf;
-        std::cout << "Receita: -" << std::fixed << std::setprecision(2) << valorTransf << std::endl;
-        std::cout << "Saldo: " << std::fixed << std::setprecision(2) << novoSaldo << std::endl;
-    default:
-        std::cout << "Digite uma ação válida";
-        break;
+        limparTela();
+
+        std::cout << "========== REGISTRO DE MOVIMENTAÇÃO ==========\n\n";
+        std::cout << "Qual é o tipo de transferência?\n\n"
+                    "1- Receita\n"
+                    "2- Despesa\n"
+                    "3- Voltar\n";
+        std::cin >> tipoTransf;
+        if (tipoTransf == 3)
+        {
+            processo = false;
+        }
+        
+        std::cout << "Digite o valor: ";
+        std::cin >> valorTransf;
+
+        switch (tipoTransf)
+        {
+        case 1:
+            novoSaldo = saldo + valorTransf;
+            std::cout << "Receita: +" << std::fixed << std::setprecision(2) << valorTransf << std::endl;
+            std::cout << "Saldo: " << std::fixed << std::setprecision(2) << novoSaldo << std::endl;
+            armazenarTransf(valorTransf, extrato);
+            break;
+        case 2:
+            novoSaldo = saldo - valorTransf;
+            std::cout << "Receita: -" << std::fixed << std::setprecision(2) << valorTransf << std::endl;
+            std::cout << "Saldo: " << std::fixed << std::setprecision(2) << novoSaldo << std::endl;
+            armazenarTransf(-valorTransf, extrato);
+            break;
+        default:
+            std::cout << "Digite uma ação válida";
+            break;
+        }
     }
+    
+    
     
     return novoSaldo;
 }
@@ -39,8 +67,10 @@ float addTransacao(float saldo) {
 void mostrarSaldo(float saldo) {
     int voltar;
 
-    std::cout << "Seu saldo: " << std::fixed << std::setprecision(2) << saldo;
-    std::cout << "\n3- Voltar\n";
+    limparTela();
+    std::cout << "========== SALDO ==========\n\n";
+    std::cout << "Seu saldo: " << std::fixed << std::setprecision(2) << saldo << std::endl;
+    std::cout << "3- Voltar\n";
     std::cin >> voltar;
     if (voltar != 2)
     {
@@ -50,31 +80,49 @@ void mostrarSaldo(float saldo) {
 }
 
 
-
 int main(){
     bool executando = true;
     int proxTela;
     float saldoAtual = 0.0;
+    std::vector<float> atualExtrato;
     
+    //roda o código
     while (executando)
     {
+        limparTela();
         std::cout << " ==================== TERMINAL DE FINANÇAS ====================\n\n";
         
 
         std::cout << "1- Fazer Transação\n"
                     "2- Ver saldo\n"
-                    "3- Sair\n";
+                    "3- Ver extrato\n"
+                    "4- Sair\n";
+
         std::cin >> proxTela;
     
         switch (proxTela)
         {
         case 1:
-            saldoAtual = addTransacao(saldoAtual);
+            saldoAtual = addTransacao(saldoAtual, atualExtrato);
+            
             break;
         case 2:
             mostrarSaldo(saldoAtual);
             break;
         case 3:
+            limparTela();
+            std::cout << "========== EXTRATO ==========\n\n";
+            for (float t : atualExtrato) {
+                std::cout << t << std::fixed << std::setprecision(2) << std::endl;
+                
+            }
+            
+            std::cout << "\nPressione ENTER para voltar...";
+            std::cin.ignore();
+            std::cin.get();
+            
+            break;
+        case 4:
             std::cout << "\n\n\n =========== SISTEMA FECHADO ==========";
             executando = false;
             break;
